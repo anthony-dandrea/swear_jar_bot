a9ac42807bd54e8700f7d054022f80d45371419b
Rodney Dennis <rdnydnns@gmail.com>
2014-07-02 18:27:06 -0500

    fuck you anthony

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+import os
 from flask import Flask, render_template
+from flask.ext import assets
 
 app = Flask(__name__)
+
+env = assets.Environment(app)
+
+# Tell Flask where to look for assets
+env.load_path = [
+    os.path.join(os.path.dirname(__file__), 'assets/bower_components'),
+    os.path.join(os.path.dirname(__file__), 'assets/fonts'),
+    os.path.join(os.path.dirname(__file__), 'assets/stylesheets'),
+    os.path.join(os.path.dirname(__file__), 'assets/scripts'),
+]
+
+env.register (
+    'css_all',
+    assets.Bundle(
+        'main.scss',
+        filters='sass',
+        output='css_all.css'
+    )
+)
 
 @app.route('/')
 def hello():
