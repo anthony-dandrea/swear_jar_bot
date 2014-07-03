7529d159a4670b7b532934c0af1b4eb28b67b3aa
Anthony D'Andrea <anthony.dandrea@me.com>
2014-07-02 20:12:56 -0500

    throw assets in static

--- app.py
+++ app.py
@@ -1,28 +1,8 @@
 import os
 import sqlite3
 from flask import Flask, render_template, request, g
-from flask.ext import assets
 
 app = Flask(__name__)
-
-env = assets.Environment(app)
-
-# Tell Flask where to look for assets
-env.load_path = [
-    os.path.join(os.path.dirname(__file__), 'assets/bower_components'),
-    os.path.join(os.path.dirname(__file__), 'assets/fonts'),
-    os.path.join(os.path.dirname(__file__), 'assets/stylesheets'),
-    os.path.join(os.path.dirname(__file__), 'assets/scripts'),
-]
-
-env.register (
-    'css_all',
-    assets.Bundle(
-        'main.scss',
-        filters='sass',
-        output='css_all.css'
-    )
-)
 
 app.config.from_object(__name__)
 
